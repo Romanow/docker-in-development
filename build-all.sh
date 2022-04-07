ec994ae5747b59d6af084e0778389d3e8cae10ef
Romanov Alex <alexey.romanov@vrbank.ru>
2022-04-07 15:34:34 +0400

    ready example

--- build-all.sh
+++ build-all.sh
@@ -2,6 +2,22 @@
 
 shopt -s extglob
 
+timed() {
+  end=$(date +%s)
+  dt=$(("$end" - $1))
+  dd=$(("$dt" / 86400))
+  dt2=$(("$dt" - 86400 * "$dd"))
+  dh=$(("$dt2" / 3600))
+  dt3=$(("$dt2" - 3600 * "$dh"))
+  dm=$(("$dt3" / 60))
+  ds=$(("$dt3" - 60 * "$dm"))
+
+  LC_NUMERIC=C printf "\nTotal runtime: %02d min %02d seconds\n" "$dm" "$ds"
+}
+
+start=$(date +%s)
+trap 'timed $start' EXIT
+
 for dir in ./modules/*; do
     gradlew_exist="$dir"/gradlew
     if [[ ! -f $gradlew_exist ]]; then
---
+++ build-parallel.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+shopt -s extglob
+
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+
+timed() {
+  end=$(date +%s)
+  dt=$(("$end" - $1))
+  dd=$(("$dt" / 86400))
+  dt2=$(("$dt" - 86400 * "$dd"))
+  dh=$(("$dt2" / 3600))
+  dt3=$(("$dt2" - 3600 * "$dh"))
+  dm=$(("$dt3" / 60))
+  ds=$(("$dt3" - 60 * "$dm"))
+
+  LC_NUMERIC=C printf "\nTotal runtime: %02d min %02d seconds\n" "$dm" "$ds"
+}
+
+start=$(date +%s)
+trap 'timed $start' EXIT
+
+declare -A process
+
+{
+  trap 'kill 0' SIGINT
+  for dir in ./modules/*; do
+    name="$(basename "$dir")"
+
+    gradle="$dir"/gradlew
+    if [[ ! -f $gradle ]]; then
+      continue
+    fi
+
+    "$dir"/gradlew clean build -p "$dir" >logs/"$name".log 2>&1 &
+
+    pid=$!
+    printf "=== Building module '%s' with PID %d ===\n" "$name", "$pid"
+    process[$name]=$pid
+  done
+}
+
+for name in "${!process[@]}"; do
+  pid="${process[$name]}"
+  wait $pid
+  code=$?
+
+  if [[ $code -ne 0 ]]; then
+    printf "${RED}Process [%s] with PID %d exit with code %d${NOCOLOR}\n" "$name" "$pid" "$code"
+    exit 1
+  else
+    printf "Process [%s] with PID %d successfully completed\n" "$name" "$pid"
+  fi
+done
